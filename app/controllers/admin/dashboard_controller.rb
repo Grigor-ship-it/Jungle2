b615c815e9ccc630b3cf1cbe414c5ab4acaa973b
Grigor-ship-it <gb795@hotmail.com>
2021-05-27 04:48:53 +0000

    add dashboard count with links and css

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"].to_s, password: ENV["ADMIN_PASSWORD"].to_s
   def show
+    @products = Product.all.count
+    @categories = Category.count
   end
 end
